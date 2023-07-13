89ca0432d9b7a7ecad66d18ec8c6d3f84c9e23ec
nilsonsts <contato@analisavetlab.com.br>
2023-07-13 16:08:40 -0300

    criamos a classe pages

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT *FROM tb_users");
-	echo json_encode($results);
+	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
 });
 
 $app->run();
